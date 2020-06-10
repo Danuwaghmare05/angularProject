a0be41917323f5a82dfa7a7d2a702f0b691e0bb1
Danuwaghmare <waghmaredanu@gmail.com>
2020-06-10 17:51:50 +0530

    practice

--- angularproject/src/app/app.component.ts
+++ angularproject/src/app/app.component.ts
@@ -2,6 +2,39 @@ import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-start',
-  template: '<h1>Hello Angular..!</h1>',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Hello Angular';
+
+  cityArr = ['Pune', 'Mumbai', 'Hydrabad'];
+  //console.log(cityArr);
+
+  studInfo = [
+    {
+      id: 1001,
+      name: 'Danu',
+      age: 25,
+      address: 'Pune',
+    },
+    {
+      id: 1002,
+      name: 'Nitu',
+      age: 22,
+      address: 'Nagpur',
+    },
+    {
+      id: 1003,
+      name: 'Siddhi',
+      age: 20,
+      address: 'Bhandara',
+    },
+    {
+      id: 1004,
+      name: 'Shruti',
+      age: 20,
+      address: 'Tumsar',
+    },
+  ];
+}
