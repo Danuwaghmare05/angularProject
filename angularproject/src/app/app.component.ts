521375d4ecc042d59b45d03e365fbb823520f300
Danuwaghmare <waghmaredanu@gmail.com>
2020-06-12 16:53:01 +0530

    pipes concept

--- angularproject/src/app/app.component.ts
+++ angularproject/src/app/app.component.ts
@@ -11,30 +11,30 @@ export class AppComponent {
   cityArr = ['Pune', 'Mumbai', 'Hydrabad'];
   //console.log(cityArr);
 
-  studInfo = [
+  mobParts = [
     {
       id: 1001,
-      name: 'Danu',
-      age: 25,
-      address: 'Pune',
+      name: 'Screen 5.5',
+      description: '5.5 Screen for Moto g',
+      inStock: 5,
+      price: 1200,
+      country: 'Germany',
     },
     {
       id: 1002,
-      name: 'Nitu',
-      age: 22,
-      address: 'Nagpur',
+      name: 'Screen 5',
+      description: '5 Screen for Samsung',
+      inStock: 8,
+      price: 1200,
+      country: 'India',
     },
     {
       id: 1003,
-      name: 'Siddhi',
-      age: 20,
-      address: 'Bhandara',
-    },
-    {
-      id: 1004,
-      name: 'Shruti',
-      age: 20,
-      address: 'Tumsar',
+      name: 'key panel',
+      description: 'Key panel for Nokia',
+      inStock: 0,
+      price: 1200,
+      country: 'Australia',
     },
   ];
 }
--- angularproject/src/app/app.module.ts
+++ angularproject/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Component, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserModule } from '@angular/platform-browser';
+import { SqrtPipe } from './pipes/sqrt.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SqrtPipe],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
