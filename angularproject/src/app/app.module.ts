d3e121ae1e8f3309b761e22f2a64426d1cd78ce5
Danuwaghmare <waghmaredanu@gmail.com>
2020-06-22 19:55:15 +0530

    Navigation Routing

--- angularproject/src/app/app.module.ts
+++ angularproject/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SqrtPipe } from './pipes/sqrt.pipe';
@@ -17,6 +18,18 @@ import { MobPartsComponent } from './product/mob-parts/mob-parts.component';
 import { PensComponent } from './product/pens/pens.component';
 import { BooksComponent } from './product/books/books.component';
 import { LaptopsComponent } from './product/laptops/laptops.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { PageNotFoundComponent } from './layouts/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'mobile', component: MobPartsComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'laptop', component: LaptopsComponent },
+  { path: 'pens', component: PensComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
@@ -34,8 +47,10 @@ import { LaptopsComponent } from './product/laptops/laptops.component';
     PensComponent,
     BooksComponent,
     LaptopsComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ angularproject/src/app/layouts/home/home.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: ` <h2>Welcome to my angular project...!</h2> `,
+})
+export class HomeComponent {}
