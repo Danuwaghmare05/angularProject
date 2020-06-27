1e18026149f148b3a20dcbd7e7f65683497e96f7
Danuwaghmare <waghmaredanu@gmail.com>
2020-06-27 20:17:12 +0530

    practice if,else

--- fashionmall/src/app/app.component.ts
+++ fashionmall/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-start',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'fashionmall';
+export class FirstComponent {
+  title = 'Angular Welcome ...!';
+
+  mobParts = [
+    {
+      id: 1001,
+      name: 'Screen 1',
+      instock: 0,
+      titleColor: 'pink',
+      isOnline: true,
+    },
+    {
+      id: 1002,
+      name: 'Screen 2',
+      instock: 15,
+      titleColor: 'green',
+      isOnline: false,
+    },
+    {
+      id: 1003,
+      name: 'Screen 3',
+      instock: 45,
+      titleColor: 'blue',
+      isOnline: true,
+    },
+  ];
 }
